d8d254e297fe06bff269e49eb9fb97b416bc67d1
Omar Jair Purata Funes <omarpurataf@gmail.com>
2019-09-08 20:19:19 +0100

    Added colours and OS regognition measures

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use std::process::Command;
+use colored::*;
+
+fn check_os() {
+    let output = if cfg!(target_os = "linux") {
+        println!("Checking your Linux Distribution");
+        Command::new("");
+    } else if cfg!(target_os = "windows") {
+        panic!("{}", "Configuration tool is not supported on this OS!".red());
+    };
+}
+
 fn main() {
-    println!("Hello, world!");
+
 }
