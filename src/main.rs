d551025569a281be061d1f15564c06e85027f9b4
Omar Jair Purata Funes <omarpurataf@gmail.com>
2019-09-07 23:21:32 -0500

    Added colours and OS regognition measures

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use std::process::Command;
+use colored::*;
+
+fn check_platfom() -> char {
+    let output = if cfg!(target_os = "linux") {
+
+    } else if cfg!(target_os = "windows") {
+        panic!("{}", "Configuration tool is not supported on this OS!".red());
+    };
+}
+
 fn main() {
-    println!("Hello, world!");
+
 }
