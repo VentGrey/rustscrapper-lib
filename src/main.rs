7a6f74b77ff7eda60f156b3654611916e5623dd8
Omar Jair Purata Funes <omarpurataf@gmail.com>
2019-10-06 01:10:48 -0500

    Added first menu

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::process::Command;
 use colored::Colorize;
+use scanln::scanln;
 
 fn check_platfom() {
     if cfg!(target_os = "linux") {
@@ -19,8 +20,19 @@ fn main() {
     check_platfom();
     println!("Welcome to {} (Hazardous Server Protection)", "H.S.P".green());
     println!("Please input your choice from our menu");
+    menu();
 }
 
 fn menu() -> u8 {
-    println!("")
+    println!("\t MENU 1 \t");
+    println!("\t Please choose what kind of operation you wish to perform");
+    println!("\t {}", "1- System Checking".yellow());
+    println!("\t {}", "2- Dependency Check".yellow());
+    println!("\t {}", "3- DevOps".yellow());
+    println!("\t {}", "4- User Database".yellow());
+    println!("\t {}", "5- Exit".yellow());
+    let mut input = scanln!("> ");
+    let input:u8 = input.parse().unwrap();
+
+    input
 }
