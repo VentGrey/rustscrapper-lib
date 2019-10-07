b2f7fd9b30fc32680ad6d10ec9c94affd87e60bf
Omar Jair Purata Funes <omarpurataf@gmail.com>
2019-10-06 22:40:18 -0500

    Added some more functions
    - Disk usage function and software checking

--- src/system.rs
+++ src/system.rs
@@ -1,7 +1,32 @@
-pub fn sysmenu() -> u8 {
+use scanln::scanln;
+use colored::Colorize;
+
+fn sysmenu() -> u8 {
+    println!("\t Please input your choice");
+    println!("\t 1- View Disk Usage");
+    println!("\t 2- View CPU Usage");
+    let input = scanln!("> ");
+    let input: u8 = input.parse().unwrap();
+
+    return input
+}
+
+fn dsk_usg() {
+    println!("{}","Checking for needed software...".blink())
+}
+
+fn check_software(kind: u8) {
 
 }
 
-pub fn mainsys() {
 
+
+pub fn mainsys() {
+    println!("\t System Checking Menu \t");
+    let option: u8 = sysmenu();
+
+    match option {
+        1 => dsk_usg(),
+        _ => panic!("{}", "Invalid Value, Aborting".red())
+    }
 }
