c95eda2984db2744fc58967a6bfcc94f4b17311e
Omar Jair Purata Funes <omarpurataf@gmail.com>
2019-12-18 23:09:02 -0600

    Started ordering code according to new standards

--- src/main.rs
+++ src/main.rs
@@ -22,18 +22,6 @@ fn check_platfom() {
     }
 }
 
-fn menu() -> u8 {
-    println!("\t Please choose what kind of operation you wish to perform");
-    println!("\t {}", "1- System Checking".green());
-    println!("\t {}", "2- Dependency Check".blue());
-    println!("\t {}", "3- DevOps".yellow());
-    println!("\t {}", "4- User Database".yellow());
-    println!("\t {}", "5- Exit".red());
-    let input = scanln!("> ");
-    let input: u8 = input.parse().unwrap();
-
-    input
-}
 
 fn main() {
     check_platfom();
@@ -52,9 +40,9 @@ fn main() {
                 "-h" | "--help" => {
                     println!(" Usage: rustscrapper [options] [quickruns]");
                     println!(" Options:");
-                    println!(" -h, --help\t show this help message and exit");
-                    println!(" -l, --list\t list available quickruns");
-                    println!(" -q, --quick\t perform a (safe) quick cleanup");
+                    println!(" -h, --help  \t show this help message and exit");
+                    println!(" -l, --list  \t list available quickruns");
+                    println!(" -q, --quick \t perform a (safe) quick cleanup");
                     println!(" -s, --system\t show basic system information");
                     return;
                 },
@@ -67,18 +55,5 @@ fn main() {
         },
 
         _ => panic!("Invalid arguments or length")
-    }
-
-
-
-    println!("Welcome to {}", "RustScrapper".red());
-    println!("Please input your choice from our menu");
-    let option = menu();
-
-    match option {
-        1 => system::mainsys(),
-        2 => deps::check_deps(),
-        5 => {},
-        _ => panic!("{}", "Invalid Value, Aborting".red()),
     }
 }
