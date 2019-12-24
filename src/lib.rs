1e6eb2e7a8df2a1496ab1709c6b630f5c963f60d
Omar Jair Purata Funes <omarpurataf@gmail.com>
2019-12-24 01:08:53 -0600

    Added a binary existing checking crate

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,8 @@
 // This is the main module for rustscrapper
+use cmd_lib::run_fun;
+use cmd_lib::FunResult;
+
+pub fn bin_exists(bin: &str) -> FunResult {
+    let result = run_fun!("which {}", bin);
+    result
+}
