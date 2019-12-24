f41e8d391634f9495a496ed4274413e9ae8c9eab
Omar Jair Purata Funes <omarpurataf@gmail.com>
2019-12-24 01:18:39 -0600

    Added an exec status function

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,27 @@
 // This is the main module for rustscrapper
 use cmd_lib::run_fun;
 use cmd_lib::FunResult;
+use cmd_lib::run_cmd;
 
 pub fn bin_exists(bin: &str) -> FunResult {
     let result = run_fun!("which {}", bin);
     result
+}
+
+pub fn exec_status(process: &str) -> i32 {
+    let result = run_cmd!(process);
+
+    let result = match result {
+        Ok(_) => {
+            0
+        }
+        Err(e) => {
+            println!("Process failed to execute at {}", e);
+            1
+        }
+    };
+
+    // If matching fails we'll assume everyting went ok
+    0
+
 }
