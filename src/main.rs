b5841beb516d0ba62a5d6082e9081c4682c17405
VentGrey <omarpurataf@gmail.com>
2020-12-27 23:23:19 -0600

    Added a new apt cleanup thing

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,11 @@ use cmd_lib::{FunResult, run_cmd, run_fun};
 /* System libraries */
 use std::{env, process::exit};
 
+/* self modules */
+mod apt;
 
 fn main() {
-    /* Handle arguments passed in order */
-
+    /* TODO: Handle arguments passed in order */
     let args: Vec<String> = env::args().collect();
+
 }
